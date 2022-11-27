d1b69f49bd1f06c436974a1d63104bcadce48d04
Dranvius <dranvius12@hotmail.com>
2022-11-27 15:37:20 -0500

    EndPart

--- baraja.js
+++ baraja.js
@@ -61,3 +61,143 @@ baraja.forEach((valor,clave)=> {
 
 
 console.log(cartas());
+
+/*
+!Area de test
+*/
+
+
+// let mazoTest = mazo();
+
+// let testEnte = [];
+// console.log(mazoTest);
+
+// testEnte.push(mazoTest[0])
+// testEnte.push(mazoTest[13]);
+
+// console.log(testEnte);
+
+// async function test(arr, ente) {
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     //*Recorrido de las cartas por medio del array dado
+
+//     if (arr[i].valor == 11 || arr[i].valor == 12 || arr[i].valor == 13) {
+//       //!Si el valor de la carta es 11 12 13 (k,q,j) sume diez
+
+//       sum += 10;
+//     } else if (arr[i].valor == 1) {
+//       //!Valor igual a uno es un AS
+
+//       if (ente == "player") {
+
+
+//         console.time("Tiempo" + i);
+
+//         const propiedades = {
+//           title: "Usted tiene un AS",
+//           icon: "question",
+//           text: "¿ Qué valor deseas tomar?",
+//           confirmButtonText: "Gamblind",
+//           footer: "<center>Consejo del día<br><br>NO MIRES ATRAS</center>",
+//           allowOutsideClick: false, //!Propiedad que permite que la ventana
+//           allowEscapeKey: false, //!No permite que otros eventos por tecla funcione
+//           allowEnterKey: true, //!Bloque la función de ENTER en el popUp
+//           stopKeydownPropagation: false,
+//           customClass: {
+//             title: "titulo-pop",
+//             text: "text-pop",
+//           },
+//           width: "20%",
+//           confirmButtonColor: "red",
+//           input: "radio",
+//           inputOptions: new Promise((resolve) => {
+//             setTimeout(() => {
+//               resolve({
+//                 1: "1",
+//                 11: "11",
+//               });
+//             }, 1000);
+//           }),
+//           inputValidator: (value) => {
+//             if (!value) {
+//               return "Necesitas seleccionar algo primero!";
+//             }
+//           },
+//         };
+
+//         let { value: valor } = await swal.fire(propiedades);
+
+//         console.log(valor);
+
+//         sum += parseInt(valor);
+
+//         console.timeEnd("Tiempo" + i);
+
+//       } else {
+
+//         //*Comportamiento para bot
+//         sum += botLogic(sum);
+//       }
+//     } else {
+//       sum += arr[i].valor;
+//     }
+//   }
+
+//   if (sum == 21) {
+//     console.log("!  Fin del juego   !El ganador es : " + ente);
+//   }
+
+//   console.log(sum)
+// }
+
+
+//LISTO  PASE A LA FUNCIÓN QUE LE DEVUELVA UN NUMERO POR OPERACIÓN
+
+// async function popUp(propiedades) {
+//   let { value: valor } = await swal.fire(propiedades).then(function (valor){
+//     return valor;
+//   });
+// }
+
+// test(testEnte,'player');
+
+/* inputOptions can be an object or Promise */
+// async function demm(){
+
+//   const inputOptions = new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         '#ff0000': 'Red',
+//         '#00ff00': 'Green',
+//         '#0000ff': 'Blue'
+//       })
+//     }, 1000)
+//   })
+
+//   const { value: color } = await Swal.fire({
+//     title: 'Select color',
+//     input: 'radio',
+//     inputOptions: inputOptions,
+//     inputValidator: (value) => {
+//       if (!value) {
+//         return 'You need to choose something!'
+//       }
+//     }
+//   })
+
+//   if (color) {
+//     Swal.fire({ html: `You selected: ${color}` })
+//   }
+
+// }
+
+// demm();
+
+
+
+// console.log(popUp(testEnte).then(function (value){
+//   console.log(value);
+
+// }));
--- logic.js
+++ logic.js
@@ -12,6 +12,20 @@ function Carta(tipo, valor, img) {
   this.img = img;
 }
 
+const endGame = (Ente) => {
+
+  swal.fire({
+    title: 'El ganador es : '+Ente,
+    text: 'Buen juego',
+    width: "20%",
+    confirmButtonColor: "red",
+    icon: "warning"
+  })
+  
+  setTimeout(reset(),3000)
+
+}
+
 function cleanBoard() {
   let etiqueta = document.getElementById("bot-Baraja");
   let especificoInterno = document.getElementById("cart");
@@ -31,15 +45,6 @@ function botLogic(sumP) {
     console.log("Salida cuando el bot tiene unv alor mayor o igual a 1");
 
     return 1;
-  }
-}
-
-function asOption(ente) {
-  if (ente == "player") {
-    Swal.fire({
-      template: "#my-template",
-    });
-  } else {
   }
 }
 
@@ -66,7 +71,8 @@ function darOne(array, index, persona, borrados = null) {
 
   if (indecesCart == 1) {
     clon.className = persona + indecesCart;
-    document.getElementById("Carta-front" + indecesCart).src = "img/Card_Model.png";
+    document.getElementById("Carta-front" + indecesCart).src =
+      "img/Card_Model.png";
   }
 }
 
@@ -121,7 +127,7 @@ function pedir(arr, baraja, ente, cartaExample) {
 
   let clon = cartaExample.cloneNode(true);
 
-    indecesCart += 1;
+  indecesCart += 1;
 
   let place =
     ente == "player"
@@ -131,11 +137,17 @@ function pedir(arr, baraja, ente, cartaExample) {
   clon.className = ente;
 
   place.appendChild(clon);
-  document.getElementById("Carta-front").id = document.getElementById("Carta-front").id;
+  document.getElementById("Carta-front").id =
+    document.getElementById("Carta-front").id;
   document.getElementById("jugador").style.justifyContent = "space-between";
-  document.getElementById("Carta-front").id = document.getElementById("Carta-front").id + indecesCart;
-  document.getElementById("Carta-front" + indecesCart).src = "img" + arr[arr.length-1].img + ".png";
+  document.getElementById("Carta-front").id =
+    document.getElementById("Carta-front").id + indecesCart;
+  document.getElementById("Carta-front" + indecesCart).src =
+    "img" + arr[arr.length - 1].img + ".png";
 
+  sumArray(arr,ente).then(valor =>{
+    ponerSumEnTablero(valor,ente)
+  })
 
 }
 
@@ -178,8 +190,6 @@ function mazo() {
       }
     }
 
-    console.log(type);
-
     if (i == 0) {
       type = "/picaishont";
     } else if (i == 1) {
@@ -192,7 +202,44 @@ function mazo() {
   return mazoR;
 }
 
-async function sumArray(arr, ente) { //!Problema
+function ponerSumEnTablero(sum,ente){ //!Hay que acomodar el puntaje para el bot cuando se presione me quedo
+
+
+  if(document.getElementById('hollow') == null){
+
+    // document.getElementById('playerScore').removeChild(document.getElementById(''));
+    child.innerText = 'PLAYER : ' +sum;
+
+  }else{
+
+    document.getElementById('playerScore').removeChild(document.getElementById('hollow'))
+    let place = '';
+    let child =  '';
+
+    if(ente = 'player'){
+
+      place = document.getElementById('playerScore');
+      child = document.createElement('h6');
+      child.innerText = 'PLAYER : ' +sum;
+      console.log(sum);
+
+      place.appendChild(child);
+
+    }else{
+
+      place = document.getElementById();
+      console.log(sum);
+
+    }
+
+
+  }
+  
+
+}
+
+
+async function sumArray(arr, ente) {
   let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
@@ -200,47 +247,46 @@ async function sumArray(arr, ente) { //!Problema
       sum += 10;
     } else if (arr[i].valor == 1) {
       if (ente == "player") {
-        //!Ejecutar un mensaje que permita escoger a usuario una opcion para el As
-        //?Ejecutar logica PLAYER
-        //*Ejecuta el PoP de selección
-        sum += setTimeout(async () => {
-          
-          const inputOptions = new Promise((resolve) => {
+        console.time("Tiempo" + i);
+
+
+          const propiedades = {
+          title: "Usted tiene un AS",
+          icon: "question",
+          text: "¿ Qué valor deseas tomar?",
+          confirmButtonText: "Gamblind",
+          footer: "<center>Consejo del día<br><br>NO MIRES ATRAS</center>",
+          allowOutsideClick: false, //!Propiedad que permite que la ventana
+          allowEscapeKey: false, //!No permite que otros eventos por tecla funcione
+          allowEnterKey: true, //!Bloque la función de ENTER en el popUp
+          stopKeydownPropagation: false,
+          customClass: {
+            title: "titulo-pop",
+            text: "text-pop",
+          },
+          width: "20%",
+          confirmButtonColor: "red",
+          input: "radio",
+          inputOptions: new Promise((resolve) => {
             setTimeout(() => {
               resolve({
                 1: "1",
                 11: "11",
               });
             }, 1000);
-          });
-        
-          const { value: valor } = await Swal.fire({
-            title: "Usted tiene un AS",
-            icon: "question",
-            text: "¿ Qué valor deseas tomar?",
-            confirmButtonText: "Gamblind",
-            footer: "<center>Consejo del día<br><br>NO MIRES ATRAS</center>",
-            allowOutsideClick: false, //!Propiedad que permite que la ventana
-            allowEscapeKey: false, //!No permite que otros eventos por tecla funcione
-            allowEnterKey: true, //!Bloque la función de ENTER en el popUp
-            stopKeydownPropagation: false,
-            customClass: {
-              title: "titulo-pop",
-              text: "text-pop",
-            },
-            width: "20%",
-            confirmButtonColor: "red",
-            input: "radio",
-            inputOptions: inputOptions,
-            inputValidator: (value) => {
-              if (!value) {
-                return "Necesitas seleccionar algo primero!";
-              }
-            },
-          });
+          }),
+          inputValidator: (value) => {
+            if (!value) {
+              return "Necesitas seleccionar algo primero!";
+            }
+          },
+          };
 
+          let { value: valor } = await swal.fire(propiedades);
 
-        }, 5000); //!Devulver el valor que desea el usuario //!Toma solo 1
+          sum += parseInt(valor);
+
+        console.timeEnd("Tiempo" + i);
       } else {
         sum += botLogic(sum);
       }
@@ -250,14 +296,13 @@ async function sumArray(arr, ente) { //!Problema
   }
 
   if (sum == 21) {
-    console.log("!  Fin del juego   !El ganador es : " + ente);
+    endGame(ente);
   }
 
   return sum;
 }
 
 //!Funciones para barrajear la baraja
-
 //! ###### 1 ######### Función de barajeo
 function fisherYatesShuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
@@ -284,6 +329,10 @@ function darInicio(array, playerOne, playerTwo) {
       playerTwo.push(array.shift());
     }
   }
+}
+
+const quedarse = () =>{
+
 }
 
 //!Funcion para iniciar el juego
@@ -360,12 +409,16 @@ function new_game() {
   c.innerText = "OMG";
 
   a.addEventListener("click", (_) => {
-    pedir(player, mazoVirgen, 'player',borrados);
+    pedir(player, mazoVirgen, "player", borrados);
   }); //!Hello
 
   c.addEventListener("click", (_) => {
     location.reload();
   }); //!Hello
+
+  b.addEventListener("click", (_) =>{
+    
+  });
 
   //Ingresar botones dentro de la etiqueta
 
@@ -403,18 +456,21 @@ function new_game() {
 
   //Dar carta a los dos jugadores
 
-  console.log(mazoVirgen);
-
   darInicio(mazoVirgen, bot, player);
-
-
 
   //Establecer suma en el tablero
 
   let scoreBot = sumArray(bot, "bot");
   let scorePlayer = sumArray(player, "player");
-  console.log(scoreBot);
-  console.log(scorePlayer);
+
+  
+
+  scoreBot.then(valor => console.log(valor), error => console.log(error));
+
+  scorePlayer.then( valor =>  ponerSumEnTablero(valor,'player'), error => console.log(error));
+
+
+ 
 
   //?Buscar como reducir la logica de ejecución
 
@@ -435,7 +491,6 @@ function new_game() {
   }, 5500);
 
   //Realizar Suma por sus valores determinados
-
- 
-
 }
+
+
